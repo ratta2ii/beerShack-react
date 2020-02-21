0fafdbc522e4ad468bf6fbccbddd9a107c3b8341
Trevor Mackin <tismetrm@gmail.com>
2020-02-21 10:16:42 -0800

    Remove hardcoded beer products

--- src/components/BeerList.jsx
+++ src/components/BeerList.jsx
@@ -1,72 +1,38 @@
 import React from 'react';
 import Beer from './Beer';
+import PropTypes from 'prop-types';
+import Navbar from './Navbar';
 
-var masterBeerList = [
-  {
-    names: 'Blue Moon',
-    brand: 'Molson Coors',
-    alcoholContent: '3.2%',
-    pintsRemaining: 124,
-    price: '$7.00',
-    imageUrl: '"https://source.unsplash.com/random/200×200/?beer"'
-  },
-  {
-    names: 'Beer 2',
-    brand: 'Molson Coors',
-    alcoholContent: '3.2%',
-    pintsRemaining: 124,
-    price: '$7.00'
-  },
-  {
-    names: 'Beer 3',
-    brand: 'Molson Coors',
-    alcoholContent: '3.2%',
-    pintsRemaining: 124,
-    price: '$7.00',
-    imageUrl: '"https://source.unsplash.com/random/300×300/?beer"'
-  },
-  {
-    names: 'Beer 4',
-    brand: 'Molson Coors',
-    alcoholContent: '3.2%',
-    pintsRemaining: 124,
-    price: '$7.00',
-    imageUrl: '"https://source.unsplash.com/random/400×400/?beer"'
-  },
-  {
-    names: 'Beer 5',
-    brand: 'Molson Coors',
-    alcoholContent: '3.2%',
-    pintsRemaining: 124,
-    price: '$7.00',
-    imageUrl: '"https://source.unsplash.com/random/100×100/?beer"'
-  },
-  {
-    names: 'Beer 6',
-    brand: 'Molson Coors',
-    alcoholContent: '3.2%',
-    pintsRemaining: 124,
-    price: '$7.00',
-    imageUrl: '"https://source.unsplash.com/random/350×350/?beer"'
-  }
-];
 
-function BeerList() {
+
+function BeerList(props) {
+
+  let productContainer = {
+    width: '50%',
+    margin: '0 auto'
+  };
+
   return (
     <div>
-      {masterBeerList.map((beer, index) =>
-        <Beer
-          names={beer.names}
-          brand={beer.brand}
-          alcoholContent={beer.alcoholContent}
-          pintsRemaining={beer.pintsRemaining}
-          price={beer.price} 
-          key={index} />
-      )}
+      <Navbar />
+      <div style={productContainer}>
+        {props.beerList.map((beer, index) =>
+          <Beer
+            names={beer.names}
+            brand={beer.brand}
+            alcoholContent={beer.alcoholContent}
+            // pintsRemaining={beer.pintsRemaining}
+            price={beer.price}
+            key={index} />
+        )}
+      </div>
     </div>
-
   );
 }
+
+BeerList.propTypes = {
+  beerList: PropTypes.array
+};
 
 
 export default BeerList;
