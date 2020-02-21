88de65425beb3de56f8fc675f3cc2110dd1afc7c
Trevor Mackin <tismetrm@gmail.com>
2020-02-21 12:43:54 -0800

    Scale down form box size Combine FormBox component and AndNewBeer Form Waste an hour looking form typo

--- src/components/NewBeerForm.jsx
+++ src/components/NewBeerForm.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import redTheme from '../assets/images/red.jpg';
 import Navbar from './Navbar';
 
 
 let formContainer = {
   backgroundColor: 'white',
-  width: '500px',
+  width: '375px',
   margin: '0 auto',
-  height: '650px',
+  height: '525px',
   borderRadius: '3px',
-  padding: '40px',
+  padding: '20px 40px 40px 40px',
   border: '60px solid #ffc150',
   webkitBoxhadow: '5px 5px 15px 9px rgb(15, 12, 12)',
   boxShadow: '5px 5px 15px 9px rgb(15, 12, 12)'
@@ -19,19 +18,19 @@ let formContainer = {
 let inputField = {
   border: '4px solid #dda832',
   width: '94%',
-  height: '60px',
-  margin: '0 0 35px 0',
+  height: '50px',
+  margin: '0 0 25px 0',
   borderRadius: '8px',
   fontSize: '20px',
   padding: '0 10px'
 };
 
 let submitButton = {
-  margin: '30px 0 50px 35%',
-  fontSize: '30px',
+  margin: '10px 0 20px 30%',
+  fontSize: '23px',
   backgroundColor: 'rgb(203, 213, 217)',
   borderRadius: '5px',
-  padding: '6px 17px',
+  padding: '4px 14px',
   border: '2px solid gray'
 };
 
@@ -67,28 +66,27 @@ function NewBeerForm(props) {
           <input style={inputField}
             type='text'
             id='names'
-            placeholder='Enter Beer Name'
+            // placeholder='Enter Beer Name'
             ref={(input) => { _names = input; }} />
 
           <p style={formLabel}>Beer Brand</p>
           <input style={inputField}
             type='text'
             id='brand'
-            placeholder='Enter Brand of Beer'
             ref={(input) => { _brand = input; }} />
 
           <p style={formLabel}>Alcohol Content</p>
           <input style={inputField}
             type='text'
             id='alcoholContent'
-            placeholder='Enter Alcohol Content'
+            // placeholder='Enter Alcohol Content'
             ref={(input) => { _alcoholContent = input; }} />
 
           <p style={formLabel}>Beer Price</p>
           <input style={inputField}
             type='text'
             id='price'
-            placeholder='Enter Price'
+            // placeholder='Enter Price'
             ref={(input) => { _price = input; }} />
 
           <div>
